ca72ad3a4ffa063294ec62d6b01bc6a3264808ad
designerfx <matthewreingold@pm.me>
2024-07-03 11:26:37 -0500

    Create trap count list.sql

---
+++ trap count list.sql
@@ -0,0 +1,33 @@
+-- this is an improvement on solarwinds diagnostics for nosiest traps
+
+--this is the top part of the query
+SELECT n.caption
+, e.NodeID, COUNT(e.LogEntryID) as total
+, n.Vendor, n.MachineType
+, n.IP_Address
+, n.DetailsUrl as [_Linkfor_caption]
+FROM Orion.OLM.LogEntry as e
+INNER JOIN Orion.OLM.LogEntryType as t on t.LogEntryTypeID = e.LogEntryTypeID
+INNER JOIN Orion.Nodes as n on n.NodeID = e.NodeID
+where t.Type = 'Traps'
+group by e.NodeID
+order by total desc
+
+-- this is the searchable part of the query
+
+SELECT n.caption
+, e.NodeID, COUNT(e.LogEntryID) as total
+, n.Vendor,
+ n.MachineType
+, n.IP_Address
+, n.DetailsUrl as [_Linkfor_caption]
+FROM Orion.OLM.LogEntry as e
+INNER JOIN Orion.OLM.LogEntryType as t on t.LogEntryTypeID = e.LogEntryTypeID
+INNER JOIN Orion.Nodes as n on n.NodeID = e.NodeID
+where t.Type = 'Traps' AND 
+(n.caption LIKE '%${SEARCH_STRING}%'  
+OR n.MachineType LIKE '%${SEARCH_STRING}%' 
+OR n.IP_Address LIKE '%${SEARCH_STRING}%'  
+OR n.Vendor LIKE '%${SEARCH_STRING}%' )
+group by e.NodeID
+order by total desc
