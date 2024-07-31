938d488d08375a9a3daea4ac74c1ed87e83ad727
designerfx <matthewreingold@pm.me>
2024-07-31 10:33:43 -0500

    Create db list and search.sql

---
+++ widgets/db list and search.sql
@@ -0,0 +1,23 @@
+﻿SELECT OAS.Node.Caption as [Node] -- node name  
+, OAS.Node.DetailsUrl as [_LinkFor_Node] -- mouse hover for node  
+, OAS.Databases.DisplayName as [DB] -- DB name  
+,OAS.Databases.DetailsUrl as [_LinkFor_DB] -- mouse hover for DB info  
+, DisplayName as [Application] --application name  
+, DetailsURL as [_LinkFor_Application] --mouse hover for application name  
+FROM Orion.APM.SqlServerApplication OAS   
+-- OAS (Orion.APM.SqlServerApplication) created to link   
+Order By Node Asc  
+
+
+SELECT OAS.Node.Caption as [Node]  
+, OAS.Node.DetailsUrl as [_LinkFor_Node]  
+, OAS.Databases.DisplayName as [DB]  
+,OAS.Databases.DetailsUrl as [_LinkFor_DB]  
+, DisplayName as [Application]  
+, DetailsURL as [_LinkFor_Application]  
+-- anything that matches a WHERE as  '${SEARCH_STRING}'  will filter  
+FROM Orion.APM.SqlServerApplication OAS  
+WHERE (DisplayName Like  '${SEARCH_STRING}'   
+OR OAS.Node.Caption Like  '${SEARCH_STRING}'  
+OR OAS.Databases.DisplayName Like  '${SEARCH_STRING}')  
+Order By Displayname Asc  
