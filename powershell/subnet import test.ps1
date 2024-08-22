2d5e988baf33b05ad23b8d85c6d807c0acb971a4
designerfx <matthewreingold@pm.me>
2024-08-22 12:51:47 -0500

    Update subnet import test.ps1

--- powershell/subnet import test.ps1
+++ powershell/subnet import test.ps1
@@ -1,17 +1,21 @@
 <#------------- FUNCTIONS -------------#>
+Function Convert-CidrToSubnetMask {
+    param (
+        [int]$cidr
+    )
+    $binaryMask = ('1' * $cidr).PadRight(32, '0')
+    $subnetMask = [convert]::ToInt32($binaryMask.Substring(0, 8), 2).ToString() + '.' +
+                  [convert]::ToInt32($binaryMask.Substring(8, 8), 2).ToString() + '.' +
+                  [convert]::ToInt32($binaryMask.Substring(16, 8), 2).ToString() + '.' +
+                  [convert]::ToInt32($binaryMask.Substring(24, 8), 2).ToString()
+    return $subnetMask
+}
 
-# Function to set up the SWIS connection
 Function Set-SwisConnection {  
     Param(  
-        [Parameter(Mandatory=$true, HelpMessage = "What SolarWinds server are you connecting to (Hostname or IP)?" ) ] 
-        [string] $solarWindsServer,  
-        
-        [Parameter(Mandatory=$true, HelpMessage = "Do you want to use the credentials from PowerShell [Trusted], or a new login [Explicit]?" ) ] 
-        [ ValidateSet( 'Trusted', 'Explicit' ) ] 
-        [ string ] $connectionType,  
-        
-        [Parameter(HelpMessage = "Which credentials should we use for an explicit logon type" ) ] 
-        $creds
+        [Parameter(Mandatory=$true, HelpMessage = "What SolarWinds server are you connecting to (Hostname or IP)?" ) ] [string] $solarWindsServer,  
+        [Parameter(Mandatory=$true, HelpMessage = "Do you want to use the credentials from PowerShell [Trusted], or a new login [Explicit]?" ) ] [ ValidateSet( 'Trusted', 'Explicit' ) ] [ string ] $connectionType,  
+        [Parameter(HelpMessage = "Which credentials should we use for an explicit logon type" ) ] $creds
     )  
      
     IF ( $connectionType -eq 'Trusted'  ) {  
@@ -26,158 +30,126 @@ Function Set-SwisConnection {
     RETURN $swis  
 }  
 
-# Function to convert CIDR to Subnet Mask
-function Convert-CidrToSubnetMask {
-    param (
-        [int]$cidr
-    )
-    $binaryMask = ('1' * $cidr).PadRight(32, '0')
-    $subnetMask = [convert]::ToInt32($binaryMask.Substring(0, 8), 2).ToString() + '.' +
-                  [convert]::ToInt32($binaryMask.Substring(8, 8), 2).ToString() + '.' +
-                  [convert]::ToInt32($binaryMask.Substring(16, 8), 2).ToString() + '.' +
-                  [convert]::ToInt32($binaryMask.Substring(24, 8), 2).ToString()
-    return $subnetMask
-}
-
-<#------------- MAIN SCRIPT -------------#>
-
+<#------------- ACTUAL SCRIPT -------------#>
 clear-host
 
-# Prompt user for hostname and connection type
-$hostname = Read-Host -Prompt "What server should we connect to?"
-$connectionType = Read-Host -Prompt "Should we use the current PowerShell credentials [Trusted], or specify credentials [Explicit]?"
+$now = Get-Date -Format "yyyyMMdd_HHmm"
+$script = $MyInvocation.MyCommand
+if($script.path){ $dir = Split-Path $script.path }
+else { $dir = [Environment]::GetFolderPath("Desktop") }
+$Logfile = "$dir\$($script.name)_$now.log"
 
-# Establish the SWIS connection using the new permissions model
-$swis = Set-SwisConnection -solarWindsServer $hostname -connectionType $connectionType
+Start-Transcript -Path $Logfile -Append -IncludeInvocationHeader
 
-# Query to get list of SNMPv3 creds in use currently
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+# Prompt for the server and connection type
+while(!$swistest) {
+    $hostname = Read-Host -Prompt "What server should we connect to?" 
+    $connectionType = Read-Host -Prompt "Should we use the current powershell credentials [Trusted], or specify credentials [Explicit]?" 
+    $swis = Set-SwisConnection $hostname $connectionType
+    $swistest = get-swisdata $swis "SELECT TOP 1 servername FROM Orion.Websites"
+}
+$swistest = $null
+
+"Connected to $hostname Successfully using $connectionType credentials"
+
+# Import addresses from csv files
+$pathtocsv = "C:\Scripts\Discovery\SampleImport.csv"
+
+# Filter subnets
+$subnets = Get-Content $pathtocsv | Select-String '^[\s|\S]+[/]+\d+' | ConvertFrom-Csv -Header subnets
+
+$discoveryName = 'Scripted Discovery'
+$autoImport = 'true'
 $query = @"
 SELECT id
 FROM Orion.Credential
-WHERE credentialowner='Orion' AND credentialtype = 'SolarWinds.Orion.Core.Models.Credentials.SnmpCredentialsV3'
+where credentialowner='Orion' and credentialtype = 'SolarWinds.Orion.Core.Models.Credentials.SnmpCredentialsV3'
 "@
 $creds = Get-SwisData $swis $query
 
-# Import addresses from CSV file
-$pathtocsv = "D:\Scripts\Discovery\SampleImport.csv"
-$importedData = Import-Csv -Path $pathtocsv -Header Column1, Column2, Column3, Column4, Column5
+# Might need to change this for your environment
+$EngineID = 1
+$DeleteProfileAfterDiscoveryCompletes = "false"
 
-# Prepare XML elements for subnets
-$subnetsElement = New-Object System.Xml.XmlDocument
-$subnetsXml = $subnetsElement.CreateElement("Subnets")
+# build the raw XML first
+$header = "<CorePluginConfigurationContext xmlns='http://schemas.solarwinds.com/2012/Orion/Core' xmlns:i='http://www.w3.org/2001/XMLSchema-instance'>"
+$subnetlist = "<Subnets>"
 
-foreach ($row in $importedData) {
-    $cidrParts = $row.Column5.Split('/')
-    $subnetIP = $cidrParts[0]
-    $cidr = [int]$cidrParts[1]
-    $subnetMask = Convert-CidrToSubnetMask -cidr $cidr
-
-    $subnet = $subnetsElement.CreateElement("Subnet")
-    $subnetIPElement = $subnetsElement.CreateElement("SubnetIP")
-    $subnetIPElement.InnerText = $subnetIP
-    $subnetMaskElement = $subnetsElement.CreateElement("SubnetMask")
-    $subnetMaskElement.InnerText = $subnetMask
-
-    $subnet.AppendChild($subnetIPElement)
-    $subnet.AppendChild($subnetMaskElement)
-    $subnetsXml.AppendChild($subnet)
+foreach ($subnet in $subnets.subnets) {
+    
+    $address = ($subnet -split '/')[0]
+    $cidr = ($subnet -split '/')[1]
+    $mask = Convert-CidrToSubnetMask $cidr
+    $subnetlist += "<AddressSubnet><SubnetIP>$($address)</SubnetIP><SubnetMask>$mask</SubnetMask></AddressSubnet>"
 }
-
-# Prepare XML elements for credentials
-$credentialsElement = New-Object System.Xml.XmlDocument
-$credentialsXml = $credentialsElement.CreateElement("Credentials")
+$subnetlist += "</Subnets>"
 
 $order = 0
+$credentials = "<Credentials>"
 foreach ($row in $creds) {
-    $order++
-    $sharedCredentialInfo = $credentialsElement.CreateElement("SharedCredentialInfo")
-    $credentialID = $credentialsElement.CreateElement("CredentialID")
-    $credentialID.InnerText = $row.id
-    $orderElement = $credentialsElement.CreateElement("Order")
-    $orderElement.InnerText = $order
-
-    $sharedCredentialInfo.AppendChild($credentialID)
-    $sharedCredentialInfo.AppendChild($orderElement)
-    $credentialsXml.AppendChild($sharedCredentialInfo)
+    $order ++
+    $credentials += "<SharedCredentialInfo><CredentialID>$($row)</CredentialID><Order>$order</Order></SharedCredentialInfo>"
 }
+$credentials += "</Credentials>"
 
-# Build CorePluginConfigurationContext
-$CorePluginConfigurationContext = New-Object System.Xml.XmlDocument
-$coreRoot = $CorePluginConfigurationContext.CreateElement("CorePluginConfigurationContext")
-$coreRoot.SetAttribute("xmlns", "http://schemas.solarwinds.com/2012/Orion/Core")
-$coreRoot.SetAttribute("xmlns:i", "http://www.w3.org/2001/XMLSchema-instance")
+$footer = @"
+<WmiRetriesCount>1</WmiRetriesCount>
+<WmiRetryIntervalMiliseconds>1000</WmiRetryIntervalMiliseconds>
+</CorePluginConfigurationContext>
+"@
 
-$bulkList = $CorePluginConfigurationContext.CreateElement("BulkList")
-$coreRoot.AppendChild($bulkList)
-$coreRoot.AppendChild($subnetsXml)
-$coreRoot.AppendChild($credentialsXml)
+$CorePluginConfigurationContext = ([xml]($header + $subnetlist + $credentials + $footer)).DocumentElement
+$CorePluginConfiguration = Invoke-SwisVerb $swis Orion.Discovery CreateCorePluginConfiguration @($CorePluginConfigurationContext)
 
-$wmiRetries = $CorePluginConfigurationContext.CreateElement("WmiRetriesCount")
-$wmiRetries.InnerText = "1"
-$coreRoot.AppendChild($wmiRetries)
+$InterfacesPluginConfigurationContext = ([xml]"
+<InterfacesDiscoveryPluginContext xmlns='http://schemas.solarwinds.com/2008/Interfaces' 
+                                  xmlns:a='http://schemas.microsoft.com/2003/10/Serialization/Arrays'>
+    <AutoImportStatus>
+        <a:string>Up</a:string>
+        <a:string>Down</a:string>
+        <a:string>Shutdown</a:string>
+    </AutoImportStatus>
+    <AutoImportVirtualTypes>
+        <a:string>Virtual</a:string>
+        <a:string>Physical</a:string>
+    </AutoImportVirtualTypes>
+    <AutoImportVlanPortTypes>
+        <a:string>Trunk</a:string>
+        <a:string>Access</a:string>
+        <a:string>Unknown</a:string>
+    </AutoImportVlanPortTypes>
+    <UseDefaults>true</UseDefaults>
+</InterfacesDiscoveryPluginContext>
+").DocumentElement
 
-$wmiInterval = $CorePluginConfigurationContext.CreateElement("WmiRetryIntervalMiliseconds")
-$wmiInterval.InnerText = "1000"
-$coreRoot.AppendChild($wmiInterval)
+$InterfacesPluginConfiguration = Invoke-SwisVerb $swis Orion.NPM.Interfaces CreateInterfacesPluginConfiguration @($InterfacesPluginConfigurationContext)
 
-$CorePluginConfigurationContext.AppendChild($coreRoot)
+$StartDiscoveryContext = ([xml]"
+<StartDiscoveryContext xmlns='http://schemas.solarwinds.com/2012/Orion/Core' xmlns:i='http://www.w3.org/2001/XMLSchema-instance'>
+    <Name>$discoveryName $([DateTime]::Now)</Name>
+    <EngineId>$EngineID</EngineId>
+    <JobTimeoutSeconds>3600</JobTimeoutSeconds>
+    <SearchTimeoutMiliseconds>2000</SearchTimeoutMiliseconds>
+    <SnmpTimeoutMiliseconds>2000</SnmpTimeoutMiliseconds>
+    <SnmpRetries>1</SnmpRetries>
+    <RepeatIntervalMiliseconds>1500</RepeatIntervalMiliseconds>
+    <SnmpPort>161</SnmpPort>
+    <HopCount>0</HopCount>
+    <PreferredSnmpVersion>SNMP2c</PreferredSnmpVersion>
+    <DisableIcmp>false</DisableIcmp>
+    <AllowDuplicateNodes>false</AllowDuplicateNodes>
+    <IsAutoImport>$autoImport</IsAutoImport>
+    <IsHidden>$DeleteProfileAfterDiscoveryCompletes</IsHidden>
+    <PluginConfigurations>
+        <PluginConfiguration>
+            <PluginConfigurationItem>$($CorePluginConfiguration.InnerXml)</PluginConfigurationItem>
+            <PluginConfigurationItem>$($InterfacesPluginConfiguration.InnerXml)</PluginConfigurationItem>
+        </PluginConfiguration>
+    </PluginConfigurations>
+</StartDiscoveryContext>
+").DocumentElement
 
-$CorePluginConfiguration = Invoke-SwisVerb $swis Orion.Discovery CreateCorePluginConfiguration @($CorePluginConfigurationContext.OuterXml)
 
-# Build InterfacesPluginConfigurationContext
-$InterfacesPluginConfigurationContext = New-Object System.Xml.XmlDocument
-$interfacesRoot = $InterfacesPluginConfigurationContext.CreateElement("InterfacesDiscoveryPluginContext")
-$interfacesRoot.SetAttribute("xmlns", "http://schemas.solarwinds.com/2008/Interfaces")
-$interfacesRoot.SetAttribute("xmlns:a", "http://schemas.microsoft.com/2003/10/Serialization/Arrays")
-
-$autoImportStatus = $InterfacesPluginConfigurationContext.CreateElement("AutoImportStatus")
-$up = $InterfacesPluginConfigurationContext.CreateElement("a:string", "http://schemas.microsoft.com/2003/10/Serialization/Arrays")
-$up.InnerText = "Up"
-$down = $InterfacesPluginConfigurationContext.CreateElement("a:string", "http://schemas.microsoft.com/2003/10/Serialization/Arrays")
-$down.InnerText = "Down"
-$shutdown = $InterfacesPluginConfigurationContext.CreateElement("a:string", "http://schemas.microsoft.com/2003/10/Serialization/Arrays")
-$shutdown.InnerText = "Shutdown"
-$autoImportStatus.AppendChild($up)
-$autoImportStatus.AppendChild($down)
-$autoImportStatus.AppendChild($shutdown)
-
-$interfacesRoot.AppendChild($autoImportStatus)
-$InterfacesPluginConfigurationContext.AppendChild($interfacesRoot)
-
-$InterfacesPluginConfiguration = Invoke-SwisVerb $swis Orion.NPM.Interfaces CreateInterfacesPluginConfiguration @($InterfacesPluginConfigurationContext.OuterXml)
-
-# Build StartDiscoveryContext
-$StartDiscoveryContext = New-Object System.Xml.XmlDocument
-$startDiscoveryRoot = $StartDiscoveryContext.CreateElement("StartDiscoveryContext")
-$startDiscoveryRoot.SetAttribute("xmlns", "http://schemas.solarwinds.com/2012/Orion/Core")
-$startDiscoveryRoot.SetAttribute("xmlns:i", "http://www.w3.org/2001/XMLSchema-instance")
-
-$nameElement = $StartDiscoveryContext.CreateElement("Name")
-$nameElement.InnerText = "$discoveryName $([DateTime]::Now)"
-$startDiscoveryRoot.AppendChild($nameElement)
-
-$engineIdElement = $StartDiscoveryContext.CreateElement("EngineId")
-$engineIdElement.InnerText = "$EngineID"
-$startDiscoveryRoot.AppendChild($engineIdElement)
-
-$timeoutElement = $StartDiscoveryContext.CreateElement("JobTimeoutSeconds")
-$timeoutElement.InnerText = "3600"
-$startDiscoveryRoot.AppendChild($timeoutElement)
-
-$pluginConfigurations = $StartDiscoveryContext.CreateElement("PluginConfigurations")
-$pluginConfiguration = $StartDiscoveryContext.CreateElement("PluginConfiguration")
-$pluginConfigurationItem1 = $StartDiscoveryContext.CreateElement("PluginConfigurationItem")
-$pluginConfigurationItem1.InnerXml = $CorePluginConfiguration.OuterXml
-$pluginConfiguration.AppendChild($pluginConfigurationItem1)
-
-$pluginConfigurationItem2 = $StartDiscoveryContext.CreateElement("PluginConfigurationItem")
-$pluginConfigurationItem2.InnerXml = $InterfacesPluginConfiguration.OuterXml
-$pluginConfiguration.AppendChild($pluginConfigurationItem2)
-
-$pluginConfigurations.AppendChild($pluginConfiguration)
-$startDiscoveryRoot.AppendChild($pluginConfigurations)
-$StartDiscoveryContext.AppendChild($startDiscoveryRoot)
-
-$DiscoveryProfileID = (Invoke-SwisVerb $swis Orion.Discovery StartDiscovery @($StartDiscoveryContext.OuterXml)).InnerText
-
-Write-Host "Discovery started. Profile ID: $DiscoveryProfileID"
+$DiscoveryProfileID = (Invoke-SwisVerb $swis Orion.Discovery StartDiscovery @($StartDiscoveryContext)).InnerText
