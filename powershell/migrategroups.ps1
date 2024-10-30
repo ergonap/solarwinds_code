2028ab7e41e3fa8f3b6027336e3308af02605da9
designerfx <matthewreingold@pm.me>
2024-10-30 15:27:36 -0500

    v2

--- powershell/migrategroups.ps1
+++ powershell/migrategroups.ps1
@@ -12,13 +12,14 @@ $SwisDest = Connect-Swis -Hostname $HostnameNew -Credential $SwisCredentialsNew
 
 <#------------- ACTUAL SCRIPT -------------#>
 
-# Get Group IDs and Names for enabled groups
-$GroupIDs = Get-SwisData -SwisConnection $SwisSource -Query "SELECT ContainerID, Name FROM Orion.Container"
+# Get Group details
+$Groups = Get-SwisData -SwisConnection $SwisSource -Query "SELECT ContainerID, Name, Description FROM Orion.Container"
 
 # Migrate the groups
-foreach ($Group in $GroupIDs) {
+foreach ($Group in $Groups) {
     $GroupID = $Group.ContainerID
     $GroupName = $Group.Name
+    $GroupDescription = $Group.Description
 
     # Check if the group already exists in the destination
     $ExistingGroup = Get-SwisData -SwisConnection $SwisDest -Query "SELECT Name FROM Orion.Container WHERE Name = '$GroupName'"
@@ -26,9 +27,23 @@ foreach ($Group in $GroupIDs) {
     if ($ExistingGroup.Count -eq 0) { 
         Write-Output "Migrating group named: $GroupName"
         
-        # Export and import the group
-        $ExportedGroup = Invoke-SwisVerb -SwisConnection $SwisSource -EntityName Orion.Container -Verb Export -Arguments $GroupID
-        Invoke-SwisVerb -SwisConnection $SwisDest -EntityName Orion.Container -Verb Import -Arguments $ExportedGroup
+        # Get the dynamic query for the group from the source
+        $DynamicQuery = Get-SwisData -SwisConnection $SwisSource -Query "SELECT Expression FROM Orion.ContainerMemberDefinition WHERE ContainerID = $GroupID"
+
+        # Create the group in the destination
+        $NewGroupID = New-SwisObject -SwisConnection $SwisDest -EntityName "Orion.Container" -Properties @{
+            Name = $GroupName
+            Description = $GroupDescription
+            Enabled = 'true'
+        }
+
+        # Apply the dynamic query to the newly created group if there is one
+        foreach ($Query in $DynamicQuery) {
+            New-SwisObject -SwisConnection $SwisDest -EntityName "Orion.ContainerMemberDefinition" -Properties @{
+                ContainerID = $NewGroupID
+                Expression = $Query.Expression
+            }
+        }
     } else { 
         Write-Output "Group named: $GroupName already exists, skipping" 
     }
